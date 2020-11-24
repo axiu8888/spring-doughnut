4a35bdc5d5a059044192d22a2f3bd2836fa75748
dingxiuan <dingxiuan@163.com>
2020-11-24 23:08:58 +0800

    打印请求日志

--- athenapdf/sample/src/main/java/com.benefitj.athenapdf.example/AthenapdfApplication.java
+++ athenapdf/sample/src/main/java/com.benefitj.athenapdf.example/AthenapdfApplication.java
@@ -1,9 +1,11 @@
 package com.benefitj.athenapdf.example;
 
 import com.benefitj.athenapdf.spring.EnableAthenapdfConfiguration;
+import com.benefitj.spring.aop.log.EnableRequestLoggingHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableRequestLoggingHandler
 @EnableAthenapdfConfiguration
 @SpringBootApplication
 public class AthenapdfApplication {
--- athenapdf/src/main/java/com/benefitj/athenapdf/api/AthenapdfController.java
+++ athenapdf/src/main/java/com/benefitj/athenapdf/api/AthenapdfController.java
@@ -4,10 +4,13 @@ import com.benefitj.athenapdf.AthenapdfCall;
 import com.benefitj.athenapdf.AthenapdfHelper;
 import com.benefitj.core.IOUtils;
 import com.benefitj.core.IdUtils;
+import com.benefitj.spring.aop.AopWebPointCut;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,12 +19,16 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
 /**
  * 生成报告的接口
  */
+@Slf4j
 @ConditionalOnClass(RequestMapping.class)
+@AopWebPointCut
 @RestController
 @RequestMapping("/athenapdf")
 public class AthenapdfController {
@@ -42,9 +49,24 @@ public class AthenapdfController {
   @GetMapping("/create")
   public void create(HttpServletResponse response,
                      String url,
-                     String filename) {
+                     String filename) throws IOException {
+    long start = System.currentTimeMillis();
+    try {
+      if (StringUtils.isBlank(url)) {
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        response.setStatus(HttpStatus.BAD_REQUEST.value());
+        response.getWriter().write("缺少url参数");
+        return;
+      }
+      URL ignore = new URL(url);
+    } catch (MalformedURLException e) {
+      response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+      response.setStatus(HttpStatus.BAD_REQUEST.value());
+      response.getWriter().write("错误的url参数");
+      return;
+    }
     filename = StringUtils.isNoneBlank(filename) ? filename : IdUtils.uuid() + ".pdf";
-    AthenapdfCall call = athenapdfHelper.execute(new File(cacheDir)
+    AthenapdfCall call = athenapdfHelper.execute(IOUtils.mkDirs(cacheDir)
         , url
         , filename
         , null
@@ -62,6 +84,7 @@ public class AthenapdfController {
       } finally {
         // 最终删除文件
         IOUtils.deleteFile(pdf);
+        log.info("{}, 使用时长: {}", filename, (System.currentTimeMillis() - start));
       }
     }
   }

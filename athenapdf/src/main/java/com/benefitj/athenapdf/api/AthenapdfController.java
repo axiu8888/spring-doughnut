552f5268b4f9060ae7cf55a58c3897520e8464c4
dingxiuan <dingxiuan@163.com>
2020-11-25 11:34:20 +0800

    pdf生成支持断点续传

--- athenapdf/src/main/java/com/benefitj/athenapdf/api/AthenapdfController.java
+++ athenapdf/src/main/java/com/benefitj/athenapdf/api/AthenapdfController.java
@@ -2,8 +2,10 @@ package com.benefitj.athenapdf.api;
 
 import com.benefitj.athenapdf.AthenapdfCall;
 import com.benefitj.athenapdf.AthenapdfHelper;
+import com.benefitj.core.EventLoop;
 import com.benefitj.core.IOUtils;
 import com.benefitj.core.IdUtils;
+import com.benefitj.spring.BreakPointTransmissionHelper;
 import com.benefitj.spring.aop.AopWebPointCut;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
@@ -15,13 +17,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 生成报告的接口
@@ -33,11 +39,21 @@ import java.nio.charset.StandardCharsets;
 @RequestMapping("/athenapdf")
 public class AthenapdfController {
 
+  /**
+   * 缓存目录
+   */
   @Value("#{ @environment['spring.athenapdf.cache-dir'] ?: '/tmp/athenapdf-pdf/' }")
   private String cacheDir;
+  /**
+   * 延迟删除的时间，默认60秒
+   */
+  @Value("#{ @environment['spring.athenapdf.delay'] ?: 60 }")
+  private long delay;
 
   @Autowired
   private AthenapdfHelper athenapdfHelper;
+
+  private final Map<String, ScheduledFuture<?>> deleteTimers = new ConcurrentHashMap<>();
 
   /**
    * 生成PDF
@@ -47,7 +63,8 @@ public class AthenapdfController {
    * @param filename 文件名，可选
    */
   @GetMapping("/create")
-  public void create(HttpServletResponse response,
+  public void create(HttpServletRequest request,
+                     HttpServletResponse response,
                      String url,
                      String filename) throws IOException {
     long start = System.currentTimeMillis();
@@ -55,37 +72,55 @@ public class AthenapdfController {
       if (StringUtils.isBlank(url)) {
         response.setCharacterEncoding(StandardCharsets.UTF_8.name());
         response.setStatus(HttpStatus.BAD_REQUEST.value());
-        response.getWriter().write("缺少url参数");
+        response.getOutputStream().write("缺少url参数".getBytes(StandardCharsets.UTF_8));
         return;
       }
       URL ignore = new URL(url);
     } catch (MalformedURLException e) {
       response.setCharacterEncoding(StandardCharsets.UTF_8.name());
       response.setStatus(HttpStatus.BAD_REQUEST.value());
-      response.getWriter().write("错误的url参数");
+      response.getOutputStream().write("错误的url参数".getBytes(StandardCharsets.UTF_8));
       return;
     }
-    filename = StringUtils.isNoneBlank(filename) ? filename : IdUtils.uuid() + ".pdf";
-    AthenapdfCall call = athenapdfHelper.execute(IOUtils.mkDirs(cacheDir)
-        , url
-        , filename
-        , null
-    );
-    if (call.isSuccessful()) {
-      File pdf = call.getPdf();
-      response.addHeader("Content-Disposition", "attachment; filename=" +
-          new String(filename.getBytes(), StandardCharsets.ISO_8859_1));
-      response.addHeader("Content-Length", String.valueOf(pdf.length()));
-      // 写入文件
-      try (final FileInputStream fis = new FileInputStream(pdf);) {
-        IOUtils.write(fis, response.getOutputStream());
-      } catch (IOException e) {
-        e.printStackTrace();
-      } finally {
-        // 最终删除文件
-        IOUtils.deleteFile(pdf);
-        log.info("{}, 使用时长: {}", filename, (System.currentTimeMillis() - start));
+
+    filename = StringUtils.isNotBlank(filename)
+        ? (filename.endsWith(".pdf") ? filename : (filename + ".pdf"))
+        : IdUtils.uuid() + ".pdf";
+    File pdf = new File(cacheDir, filename);
+
+    // 取消删除的调度
+    cancelDeleteTimer(pdf);
+
+    try {
+      if (!pdf.exists()) {
+        AthenapdfCall call = athenapdfHelper.execute(IOUtils.mkDirs(cacheDir), url, filename, null);
+        if (!call.isSuccessful()) {
+          log.info("生成PDF失败, filename: {}, url: {}", filename, url);
+          return;
+        }
+        pdf = call.getPdf();
       }
+      BreakPointTransmissionHelper.download(request, response, pdf, filename);
+      scheduleDeleteTimer(pdf);
+    } finally {
+      // 最终删除文件
+      scheduleDeleteTimer(pdf);
+    }
+    log.info("{}, 使用时长: {}", filename, (System.currentTimeMillis() - start));
+  }
+
+  private void scheduleDeleteTimer(final File deletePdf) {
+    ScheduledFuture<?> deleteTimer = EventLoop.single().schedule(() -> {
+      IOUtils.deleteFile(deletePdf);
+      deleteTimers.remove(deletePdf.getAbsolutePath());
+    }, delay, TimeUnit.SECONDS);
+    deleteTimers.put(deletePdf.getAbsolutePath(), deleteTimer);
+  }
+
+  private void cancelDeleteTimer(File pdf) {
+    ScheduledFuture<?> deleteTimer = deleteTimers.remove(pdf.getAbsolutePath());
+    if (deleteTimer != null) {
+      deleteTimer.cancel(true);
     }
   }
 

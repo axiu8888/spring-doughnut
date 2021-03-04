45bad88361e9585c545c0522716d8e1b41a42ed7
dingxiuan <dingxiuan@163.com>
2021-03-04 14:35:32 +0800

    优化MQTT发布

--- mqtt/src/main/java/com/benefitj/spring/mqtt/MqttPublisher.java
+++ mqtt/src/main/java/com/benefitj/spring/mqtt/MqttPublisher.java
@@ -6,8 +6,14 @@ import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
 
 /**
@@ -22,33 +28,61 @@ public class MqttPublisher implements IMqttSender, InitializingBean, DisposableB
   /**
    * 前缀
    */
-  private String prefix = "mqtt-publisher-";
+  private String prefix;
+  /**
+   * 客户端数量
+   */
+  private int count;
   /**
    * 客户端
    */
-  private SingleMqttClient client;
+  private List<SingleMqttClient> clients;
+  /**
+   * 调度器
+   */
+  private ExecutorService executor;
+
+  private final AtomicInteger dispatcher = new AtomicInteger();
 
   public MqttPublisher(MqttPahoClientFactory clientFactory) {
-    this.clientFactory = clientFactory;
+    this(clientFactory, "mqtt-publisher-");
   }
 
   public MqttPublisher(MqttPahoClientFactory clientFactory, String prefix) {
+    this(clientFactory, prefix, 1);
+  }
+
+  public MqttPublisher(MqttPahoClientFactory clientFactory, String prefix, int count) {
     this.clientFactory = clientFactory;
     this.prefix = prefix;
+    this.count = count;
   }
 
   @Override
   public void afterPropertiesSet() throws Exception {
-    String clientId = IdUtils.nextLowerLetterId(getPrefix(), null, 6);
+    String id = IdUtils.nextLowerLetterId(getPrefix(), null, 4);
+    int count = this.getCount();
     // 设置自动连接
-    this.clientFactory.getConnectionOptions().setAutomaticReconnect(true);
-    this.client = new SingleMqttClient(this.clientFactory, clientId);
-    this.client.setExecutor(Executors.newSingleThreadScheduledExecutor());
+    MqttPahoClientFactory factory = this.clientFactory;
+    factory.getConnectionOptions().setAutomaticReconnect(true);
+
+    this.executor = Executors.newFixedThreadPool(Math.min(count, 20));
+
+    ScheduledExecutorService single = Executors.newSingleThreadScheduledExecutor();
+    List<SingleMqttClient> clients = new ArrayList<>(count);
+    for (int i = 1; i <= count; i++) {
+      SingleMqttClient client = new SingleMqttClient(factory, id + i);
+      client.setExecutor(single);
+      clients.add(client);
+    }
+    this.setClients(Collections.unmodifiableList(clients));
   }
 
   @Override
   public void destroy() throws Exception {
-    this.client.disconnect();
+    for (SingleMqttClient client : getClients()) {
+      client.disconnect();
+    }
   }
 
   /**
@@ -56,12 +90,17 @@ public class MqttPublisher implements IMqttSender, InitializingBean, DisposableB
    */
   @Override
   public IMqttClient getClient() {
-    return this.client;
+    int index = dispatcher.incrementAndGet();
+    SingleMqttClient client = this.clients.get(index % this.clients.size());
+    if (index > 10000) {
+      dispatcher.set(this.clients.size());
+    }
+    return client;
   }
 
   @Override
   public Executor getExecutor() {
-    return this.client.getExecutor();
+    return this.executor;
   }
 
   /**
@@ -73,6 +112,14 @@ public class MqttPublisher implements IMqttSender, InitializingBean, DisposableB
     getExecutor().execute(() -> consumer.accept(getClient()));
   }
 
+  public List<SingleMqttClient> getClients() {
+    return clients;
+  }
+
+  public void setClients(List<SingleMqttClient> clients) {
+    this.clients = clients;
+  }
+
   public String getPrefix() {
     return prefix;
   }
@@ -81,4 +128,11 @@ public class MqttPublisher implements IMqttSender, InitializingBean, DisposableB
     this.prefix = prefix;
   }
 
+  public int getCount() {
+    return count;
+  }
+
+  public void setCount(int count) {
+    this.count = count;
+  }
 }
--- mqtt/src/main/java/com/benefitj/spring/mqtt/config/MqttPublisherConfiguration.java
+++ mqtt/src/main/java/com/benefitj/spring/mqtt/config/MqttPublisherConfiguration.java
@@ -2,6 +2,7 @@ package com.benefitj.spring.mqtt.config;
 
 import com.benefitj.spring.mqtt.MqttOptionsProperty;
 import com.benefitj.spring.mqtt.MqttPublisher;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,9 @@ import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
 @Configuration
 public class MqttPublisherConfiguration extends CommonsMqttConfiguration {
 
+  @Value("#{ @environment['spring.mqtt.publisher.client-count'] ?: 1 }")
+  private Integer publisherCount = 1;
+
   /**
    * 消息发布的客户端
    */
@@ -20,7 +24,7 @@ public class MqttPublisherConfiguration extends CommonsMqttConfiguration {
   @Bean
   public MqttPublisher mqttPublisher(MqttPahoClientFactory clientFactory,
                                      MqttOptionsProperty property) {
-    return new MqttPublisher(clientFactory, property.getClientIdPrefix());
+    return new MqttPublisher(clientFactory, property.getClientIdPrefix(), publisherCount);
   }
 
 }

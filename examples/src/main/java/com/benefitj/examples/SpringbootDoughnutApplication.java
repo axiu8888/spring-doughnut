b0ecd7c67f3b1cba8891fd3947429d269194af39
dingxiuan <dingxiuan@163.com>
2024-01-15 18:51:34 +0800

    添加cache

--- examples/src/main/java/com/benefitj/examples/SpringbootDoughnutApplication.java
+++ examples/src/main/java/com/benefitj/examples/SpringbootDoughnutApplication.java
@@ -1,6 +1,5 @@
 package com.benefitj.examples;
 
-import com.benefitj.core.CatchUtils;
 import com.benefitj.spring.aop.log.EnableHttpLoggingHandler;
 import com.benefitj.spring.aop.ratelimiter.EnableRedisRateLimiter;
 import com.benefitj.spring.athenapdf.EnableAthenapdf;
@@ -9,16 +8,14 @@ import com.benefitj.spring.influxdb.spring.EnableInfluxDB;
 import com.benefitj.spring.listener.AppStateHook;
 import com.benefitj.spring.redis.EnableRedisMessageChannel;
 import com.benefitj.spring.swagger.EnableSwaggerApi;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import lombok.extern.slf4j.Slf4j;
-import org.ehcache.Cache;
-import org.ehcache.CacheManager;
-import org.ehcache.config.Configuration;
-import org.ehcache.config.builders.CacheManagerBuilder;
-import org.ehcache.xml.XmlConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.io.ClassPathResource;
+
+import java.util.concurrent.TimeUnit;
 
 
 @PropertySource(value = "classpath:/swagger-api-info.properties", encoding = "utf-8")
@@ -42,24 +39,47 @@ public class SpringbootDoughnutApplication {
         evt -> log.info("app stopped ...")
     );
 
-    AppStateHook.registerStart(evt -> testEhcache());
+//    AppStateHook.registerStart(evt -> testEhcache());
+    AppStateHook.registerStart(evt -> testCaffeine());
   }
 
-  static void testEhcache() {
-    log.info("Creating cache manager via XML resource");
-    ClassPathResource cpr = new ClassPathResource("classpath:ehcache.xml");
-    Configuration config = new XmlConfiguration(CatchUtils.tryThrow(cpr::getURL));
-    try (CacheManager cacheManager = CacheManagerBuilder.newCacheManager(config)) {
-      cacheManager.init();
+//  static void testEhcache() {
+//    log.info("Creating cache manager via XML resource");
+//    ClassPathResource cpr = new ClassPathResource("ehcache.xml");
+//    Configuration config = new XmlConfiguration(CatchUtils.tryThrow(cpr::getURL));
+//    try (CacheManager cacheManager = CacheManagerBuilder.newCacheManager(config)) {
+//      cacheManager.init();
+//
+//      Cache<Long, String> cache = cacheManager.getCache("cache", Long.class, String.class);
+//      log.info("Putting to cache");
+//      cache.put(1L, "da one!");
+//      String value = cache.get(1L);
+//      log.info("Retrieved '{}'", value);
+//      log.info("Closing cache manager");
+//    }
+//    log.info("Exiting");
+//  }
 
-      Cache<Long, String> cache = cacheManager.getCache("cache", Long.class, String.class);
-      log.info("Putting to cache");
-      cache.put(1L, "da one!");
-      String value = cache.get(1L);
-      log.info("Retrieved '{}'", value);
-      log.info("Closing cache manager");
-    }
-    log.info("Exiting");
+  static void testCaffeine() {
+    Cache<Object, Object> cache = Caffeine.newBuilder()
+        //初始数量
+        .initialCapacity(10)
+        //最大条数
+        .maximumSize(10)
+        //PS：expireAfterWrite和expireAfterAccess同时存在时，以expireAfterWrite为准。
+        // 最后一次写操作后经过指定时间过期
+        .expireAfterWrite(1, TimeUnit.SECONDS)
+        // 最后一次读或写操作后经过指定时间过期
+        .expireAfterAccess(1, TimeUnit.SECONDS)
+        //监听缓存被移除
+        .removalListener((key, val, removalCause) -> { })
+        //记录命中
+        .recordStats()
+        .build();
+
+    cache.put("1","张三");
+    System.out.println(cache.getIfPresent("1"));
+    System.out.println(cache.get("2",o -> "默认值"));
   }
 
 }
